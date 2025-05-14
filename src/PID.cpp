d1dad86ef1869edbb8194bc76b81ed1a7f4d17aa
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-05-13 19:52:37 -0700

    avoid divide by nearly zero

--- src/PID.cpp
+++ src/PID.cpp
@@ -8,7 +8,12 @@ double PID::compute(double setpoint, double actual, double delta_time)
 {
     double error = setpoint - actual;
     integral += error * delta_time;
-    double derivative = (error - prevError) / delta_time;
+    //double derivative = (error - prevError) / delta_time;
+    double derivative = 0.0;
+    if (delta_time > 1e-6)
+    {
+        derivative = (error - prevError) / delta_time;
+    }
     prevError = error;
     return kp * error + ki * integral + kd * derivative;
 }
